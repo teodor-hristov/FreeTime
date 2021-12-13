15253265b9c481c77515fdfdb87732c6dd3c34dd
Teodor Hristov <tedotedo771@gmail.com>
2021-12-13 03:03:50 +0200

    +1

--- AdventOfCode/Day 1/SonarSweep.cpp
+++ AdventOfCode/Day 1/SonarSweep.cpp
@@ -1,34 +1,79 @@
 #include <iostream>
+#include <vector>
 #include <cstring>
 
 using namespace std;
 
-int main()
-{
+static size_t getIncreasingFromFile(const char *const inputFile) {
     size_t increasedCount = 0;
     char buf[128] = {0};
     int prevNum = -1;
     int currNum = -1;
-    FILE* input = fopen("input", "r");
+    FILE *input = fopen(inputFile, "r");
     if (!input)
         return -1;
 
-    while(fgets(buf, sizeof(buf), input)){
+    while (fgets(buf, sizeof(buf), input)) {
         currNum = atoll(buf);
-        memset(buf, 0 ,sizeof(buf));
+        memset(buf, 0, sizeof(buf));
 
-        if (prevNum >= 0)
-        {
-            increasedCount += !!(prevNum <= currNum);
+        if (prevNum >= 0) {
+            increasedCount += !!(prevNum < currNum);
             prevNum = currNum;
-        }
-        else
-        {
+        } else {
             prevNum = currNum;
         }
     }
 
-    cout << "Increased: " << increasedCount << endl;
+    fclose(input);
+    return increasedCount;
+}
+
+static void generateNewInputFile(const char *const inputFile, const char *const newInputFile) {
+    char buf[128] = {0};
+    int numbers[3] = {0};
+    FILE *output = fopen(newInputFile, "w+");
+    FILE *input = fopen(inputFile, "r");
+    if (!input || !output) {
+        if (input) fclose(input);
+        if (output) fclose(output);
+        return;
+    }
+
+    fseek(output, 0, SEEK_SET);
+    fseek(input, 0, SEEK_SET);
+
+    size_t currNum = 0;
+    size_t t = 0;
+    bool firstStep = false;
+    while (fgets(buf, sizeof(buf), input)) {
+        currNum = atoll(buf);
+        memset(buf, 0, sizeof(buf));
+
+        if (t == 2 && a)
+        {
+            swap(numbers[0], numbers[1]);
+            swap(numbers[1], numbers[2]);
+        }
+
+        numbers[t] = currNum;
+
+        if (t < 2){
+            t++;
+            continue;
+        }
+
+        sprintf(buf, "%d\n", numbers[0] + numbers[1] + numbers[2]);
+        fputs(buf, output);
+        memset(buf, 0, sizeof(buf));
+        firstStep = true;
+    }
 
     fclose(input);
+    fclose(output);
+};
+
+int main() {
+    generateNewInputFile("input", "input2");
+    cout << "Increased: " << getIncreasingFromFile("input2") << endl;
 }
