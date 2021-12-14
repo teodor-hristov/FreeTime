759887b4a69a0661dadcdcff531d0c182f7876a5
Teodor Hristov <tedotedo771@gmail.com>
2021-12-14 23:20:13 +0200

    +1 Day 2 Part 2

--- AdventOfCode/Day 2/Dive.cpp
+++ AdventOfCode/Day 2/Dive.cpp
@@ -8,28 +8,74 @@ const char* const fileName = "input";
 typedef struct position{
     int horizontal;
     int vertical;
+    int aim;
 }Position_t;
 
-Position_t calculatePosition(const char* const inputFileName){
+Position_t calculatePosition_Part1(const char* const inputFileName){
     char buf[255] = {0,};
     char movement[32] = {0,};
     int movementSteps = 0;
-    Position_t position = {0,0};
+    Position_t position = {0,0,0};
     FILE* fp = fopen(fileName, "r");
     if(!fp) return {-1,-1};
 
-    while(fgets(buf, sizeof(buf), fp)){
+    while(fgets(buf, sizeof(buf), fp)){       
         sscanf(buf, "%s %d", movement, &movementSteps);
-        if (strstr(movement, "up") != nullptr) {position.horizontal -= movementSteps;}
-        if (strstr(movement, "down") != nullptr) {position.horizontal += movementSteps;}
-        if (strstr(movement, "forward") != nullptr) {position.vertical += movementSteps;}
-    }
+        if (strstr(movement, "up") != nullptr) 
+        {
+            position.vertical -= movementSteps;
+        }
+        if (strstr(movement, "down") != nullptr) 
+        {
+            position.vertical += movementSteps;
+        }
+        if (strstr(movement, "forward") != nullptr) 
+        {
+            position.horizontal += movementSteps;
+            position.vertical += movementSteps*position.aim;
+        }
+
+
+        printf("aim: %d horizontal: %d vertical: %d\n", position.aim,
+        position.horizontal, position.vertical);
+   }
+
+    return position;
+}
+
+Position_t calculatePosition_Part2(const char* const inputFileName){
+    char buf[255] = {0,};
+    char movement[32] = {0,};
+    int movementSteps = 0;
+    Position_t position = {0,0,0};
+    FILE* fp = fopen(fileName, "r");
+    if(!fp) return {-1,-1};
+
+    while(fgets(buf, sizeof(buf), fp)){       
+        sscanf(buf, "%s %d", movement, &movementSteps);
+        if (strstr(movement, "up") != nullptr) 
+        {
+            position.aim -= movementSteps;
+        }
+        if (strstr(movement, "down") != nullptr) 
+        {
+            position.aim += movementSteps;
+        }
+        if (strstr(movement, "forward") != nullptr) 
+        {
+            position.horizontal += movementSteps;
+            position.vertical += movementSteps*position.aim;
+        }
+   }
 
     return position;
 }
 
 int main(){
-    Position_t pos = calculatePosition(fileName);
-    cout << pos.horizontal * pos.vertical << endl;
+    Position_t pos1 = calculatePosition_Part1(fileName);
+    cout << "Part 1: " << pos1.horizontal * pos1.vertical << endl;
+
+    Position_t pos2 = calculatePosition_Part2(fileName);
+    cout << "Part 2: " << pos2.horizontal * pos2.vertical << endl;
 
 }
