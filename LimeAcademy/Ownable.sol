8df2868f4858542f81d502aac4add6869ed234f7
Teodor Hristov <tedotedo771@gmail.com>
2022-05-08 13:32:41 +0300

    Owner's address needs to be private

--- LimeAcademy/Ownable.sol
+++ LimeAcademy/Ownable.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.0;
 
 contract Ownable {
-    address public owner;
+    address internal owner;
 
     modifier onlyOwner() {
         require(owner == msg.sender, "Not invoked by the owner");
         _;
+    }
+
+    function getOwner() public view returns(address) {
+        return owner;
     }
 
     constructor() {
